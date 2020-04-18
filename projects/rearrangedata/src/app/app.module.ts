a1c4549f25b8cc5ec61df47d2b7ea113d2035d79
Tibing <nik.poltoratsky@gmail.com>
2020-04-18 17:04:39 +0300

    feat: rearrange data example

--- projects/rearrangedata/src/app/app.module.ts
+++ projects/rearrangedata/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
